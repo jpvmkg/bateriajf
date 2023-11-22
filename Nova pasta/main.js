6ec16476f2c0fe8f8120e07323e894233f9c898c
Samurai manso <127344521+jpvmkg@users.noreply.github.com>
2023-11-22 11:42:22 -0300

    Add files via upload

--- Nova pasta/main.js
+++ Nova pasta/main.js
@@ -1,3 +1,9 @@
 function playAudio(id_do_som){
-    alert(id_do_som)
+   const audio = document.getElementById(id_do_som);
+
+   if(audio){
+    audio.play();
+   }else{
+    alert("Não foi possivel reproduzir o som!");
+   }
 }
